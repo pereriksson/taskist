d7b35e107c0b8ed7cdd6b083b50852184ea01b9f
Per Eriksson <per@interapp.se>
2021-07-05 15:47:19 +0200

    ability to create items

--- src/Controller/AddController.php
+++ src/Controller/AddController.php
@@ -2,21 +2,78 @@
 
 namespace App\Controller;
 
+use App\Entity\Item;
+use App\Entity\Person;
+use App\Entity\Project;
+use App\Entity\Tag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AddController extends AbstractController
 {
     /**
-     * @Route("/add", name="add")
+     * @Route("/add", name="add", methods={"GET"})
      */
-    public function number(): Response
+    public function index(): Response
     {
+        $repository = $this->getDoctrine()->getRepository(Person::class);
+        $people = $repository->findAll();
+
+        $repository = $this->getDoctrine()->getRepository(Tag::class);
+        $tags = $repository->findAll();
+
+        $repository = $this->getDoctrine()->getRepository(Project::class);
+        $projects = $repository->findAll();
+
         return $this->render('index.twig', [
-            "title" => "Home",
+            "title" => "Add a todo",
             "navItems" => [],
+            "people" => $people,
+            "tags" => $tags,
+            "projects" => $projects,
             "component" => "components/add.twig"
         ]);
+    }
+
+    /**
+     * @Route("/add", name="add_process", methods={"POST"})
+     */
+    public function process(Request $request): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $item = new Item();
+        $item->setDone(false);
+        $item->setTitle($request->get("title"));
+        $item->setDescription($request->get("description"));
+
+        if ($request->get("deadline")) {
+            try {
+                $item->setDeadline(new \DateTime($request->get("deadline")));
+            } catch (\Exception $e) {
+                // Date could not be parsed, ignore
+            }
+        }
+
+        if ($request->get("project")) {
+            $repo = $this->getDoctrine()->getRepository(Project::class);
+            $project = $repo->find($request->get("project"));
+            $item->setProject($project);
+        }
+
+        if ($request->get("responsible")) {
+            $repo = $this->getDoctrine()->getRepository(Person::class);
+            $responsible = $repo->find($request->get("responsible"));
+            $item->setResponsible($responsible);
+        }
+
+        // Require a title
+        if ($item->getTitle()) {
+            $manager->persist($item);
+            $manager->flush();
+        }
+
+        return $this->redirectToRoute("home");
     }
 }
