4574247bc6aacda4f936804f033bfb5340d0253d
Per Eriksson <per@interapp.se>
2021-07-05 15:47:55 +0200

    added recursive rendering etc

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,17 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Item;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function number(): Response
+    public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Item::class);
+        $items = $repo->findBy([
+            "parent" => null
+        ]);
+
         return $this->render('index.twig', [
             "title" => "Home",
             "navItems" => [],
+            "items" => $items,
             "component" => "components/home.twig"
         ]);
     }
